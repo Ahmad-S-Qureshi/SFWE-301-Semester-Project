7944f7f5d5f4a33770439cb643aaab328d1bda6e
Ahmad-S-Qureshi <113152659+Ahmad-S-Qureshi@users.noreply.github.com>
2023-11-12 16:43:42 -0700

    Reorganized last things

--- Report_Generation/src/main/java/AnnualReportGenerator.java
+++ Report_Generation/src/main/java/AnnualReportGenerator.java
@@ -7,29 +7,31 @@ public class AnnualReportGenerator extends ReportGenerator {
     private Student studentData;
     private Scholarship scholarshipData;
     protected String filepath;
+    protected String filePrefix;
     public static int reportNumber = 0;
     public AnnualReportGenerator(Student student, Scholarship scholarship) {
         this.filepath = "src/main/AnnualReports/";
         this.studentData = student;
         this.scholarshipData = scholarship;
+        this.filePrefix = "AnnualReport";
     }
-    public String parseData() {
-        return "ANNUAL REPORT PARSING TO BE IMPLEMENTED";
-    }
-    public void writeToFile() {
+    public void writeToFile(){
         try {
-            String completeFilePath = this.filepath + "AnnualReportNumber" + reportNumber + ".txt";
+            String completeFilePath = this.filepath + filePrefix + reportNumber + ".txt";
             File newAnnualReport = new File(completeFilePath);
-            newAnnualReport.createNewFile();
-            
+            System.out.println(newAnnualReport.createNewFile());
             FileWriter ReportWriter = new FileWriter(completeFilePath);
             ReportWriter.write(parseData());
             ReportWriter.close();
             reportNumber++;
-            System.out.println("Annual Report Generated");
+            System.out.println("Report Generated");
         } catch (Exception e){
             System.out.println(e.getMessage());
         }
     }
+    public String parseData() {
+        return "ANNUAL REPORT PARSING TO BE IMPLEMENTED";
+    }
+    
     
 }
--- Report_Generation/src/main/java/ReportGenerator.java
+++ Report_Generation/src/main/java/ReportGenerator.java
@@ -1,8 +1,10 @@
 package main.java;
+
 public abstract class ReportGenerator {
     protected String filepath;
     static int reportNumber;
     abstract String parseData();
+    protected String filePrefix;
     abstract void writeToFile();
     // Makes CSV report files
     // One function for each type of report
