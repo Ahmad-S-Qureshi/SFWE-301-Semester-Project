4cd2cc5d41f7acb615c9d01fd391ce9d67954d11
jorgedr22 <104534378+jorgedr22@users.noreply.github.com>
2023-11-21 11:45:26 -0700

    updated test cases and made TestMain

---
+++ Report_Generation/src/main/java/TestMain.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import main.java.*
+
+public class TestMain {
+    public static void main(String[] args) {
+        ArrayList <Scholarship> scholarshipData = new ArrayList<Scholarship>();
+        ArrayList <Student> studentData = new ArrayList<Student>();
+
+        Scholarship scholarship1 = new Scholarship("Academic Excellence Scholarship",2500,"12-12-2025","NA","Software Engineering");
+        
+        scholarshipData.add(scholarship1);
+
+
+    }
+}
