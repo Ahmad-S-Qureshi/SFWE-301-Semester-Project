d89e5afae26e958d45861d09189d15fec4e10e4d
jennifergg1 <galvanjennifer001@gmail.com>
2023-11-21 16:12:06 -0700

    ApplicationReportGenerator
    Made ApplicationReportGenerator and made changes to ApplicationData(removd static)

--- Report_Generation/src/main/java/ApplicationData.java
+++ Report_Generation/src/main/java/ApplicationData.java
@@ -4,18 +4,10 @@ import java.util.ArrayList;
 
 public class ApplicationData {
     // Static member representing a 2D ArrayList with pairs of questions and answers
-    private static ArrayList<ArrayList<String>> applicationDataSet = new ArrayList<>();
-
-    // Static initializer block to populate the data
-    static {
-        ArrayList<String> firstPair = new ArrayList<>();
-        firstPair.add("Question");
-        firstPair.add("Answer");
-        applicationDataSet.add(firstPair);
-    }
+    private ArrayList<ArrayList<String>> applicationDataSet = new ArrayList<>();
 
     // Pair adder
-    public static void addPair(String question, String answer) {
+    public void addPair(String question, String answer) {
         ArrayList<String> pair = new ArrayList<>();
         pair.add(question);
         pair.add(answer);
@@ -23,7 +15,7 @@ public class ApplicationData {
     }
 
     // Getter for accessing the static data
-    public static ArrayList<ArrayList<String>> getApplicationDataSet() {
+    public ArrayList<ArrayList<String>> getApplicationDataSet() {
         return applicationDataSet;
     }
 }
---
+++ Report_Generation/src/main/java/ApplicationReportGenerator.java
@@ -0,0 +1,75 @@
+package main.java;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+import main.java.ApplicationData;
+import main.java.Scholarship;
+import main.java.Student;
+
+/**
+ * The AnnualReportGenerator class extends ReportGenerator and is specifically
+ * designed
+ * for generating annual reports based on scholarship data.
+ */
+public class ApplicationReportGenerator extends ReportGenerator {
+
+    /** The year for which the annual report is generated. */
+    private Scholarship scholarship;
+    private Student student;
+    private ApplicationData questions;
+
+// schlarship, studnet
+    /**
+     * Constructs an AnnualReportGenerator with the provided scholarship data and
+     * year.
+     *
+     * @param annualD The ArrayList of scholarships for the annual report.
+     * @param year    The year for which the annual report is generated.
+     */
+    public ApplicationReportGenerator(Scholarship scholarship, Student student, ApplicationData data) {
+        this.filepath = "src/Reports/ApplicationReport/";
+        this.scholarship = scholarship;
+        this.student = student;
+        this.questions = data;
+        this.filePrefix = "ApplicationReport";
+    }
+
+    /**
+     * Writes the annual report to a CSV file.
+     * Overrides the method in the superclass.
+     */
+    @Override
+    public String writeToFile() {
+        try {
+            String completeFilePath = this.filepath + filePrefix + "_" + year + ".csv";
+            File newAnnualReport = new File(completeFilePath);
+            System.out.println(newAnnualReport.createNewFile());
+            FileWriter ReportWriter = new FileWriter(completeFilePath);
+            ReportWriter.write(parseData());
+            System.out.println("Printed " + parseData());
+            ReportWriter.close();
+            System.out.println("Report Generated");
+            return completeFilePath;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * Parses scholarship data for the annual report.
+     * Overrides the method in the superclass.
+     *
+     * @return A string representation of the scholarship data in CSV format.
+     */
+    @Override
+    public String parseData() {
+        String reportString = "Question, Answer\n";
+        for(int i = 0; i < questions.getApplicationDataSet().size(); i++) {
+            reportString += questions.getApplicationDataSet().get(i).get(0) + "," + questions.getApplicationDataSet().get(i).get(1) + "\n";
+        }
+        return reportString;
+    }
+}
